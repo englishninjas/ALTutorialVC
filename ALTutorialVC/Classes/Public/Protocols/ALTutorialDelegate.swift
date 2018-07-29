9feffc58cb8e0c917f2d837b6495a7f5d8a5911b
Ali Germiyanoglu <aligermiyanoglu@gmail.com>
2018-07-29 17:53:28 +0300

    - subscription packages new UI Layout - Tutorials initial implementation

--- ALTutorialVC/Classes/Public/Protocols/ALTutorialDelegate.swift
+++ ALTutorialVC/Classes/Public/Protocols/ALTutorialDelegate.swift
@@ -18,6 +18,34 @@ import Foundation
     func tutorialViews() -> [UIView]
 }
 
+public protocol TutorialNavigationHandler {
+    func segue() -> String
+    func performTipsSegue(named: String)
+}
+
+public extension TutorialNavigationHandler where Self: UIViewController {
+    public func performTipsSegue(named: String) {
+        self.performSegue(withIdentifier: named,
+                          sender: self)
+    }
+}
+
+public protocol TutorialConsumer {
+    func tutorialSource() -> TutorialViewSource
+    func tutorialNavigationHandler() -> TutorialNavigationHandler
+    func showTutorial()
+}
+
+extension UIViewController {
+    public func visibleView() -> Bool {
+        return isViewLoaded && view.window != nil
+    }
+    
+    public func toggleUserInteraction(_ enabled: Bool) {
+        view.isUserInteractionEnabled = enabled
+        tabBarController?.view.isUserInteractionEnabled = enabled
+    }
+}
 
 /*
  Protocol for VCs which will be introduced
@@ -42,3 +70,38 @@ import Foundation
     */
     func shouldTutorialStart() -> Bool
 }
+
+public extension TutorialDelegate where Self: UIViewController {
+    
+    public func tutorialDidTimeout() {
+        toggleUserInteraction(true)
+    }
+    
+    public func tutorialWillStart() {
+        toggleUserInteraction(false)
+    }
+    
+    public func tutorialDidStart() {
+        toggleUserInteraction(true)
+    }
+    
+    public func tutorialDidEnd() {
+        toggleUserInteraction(true)
+    }
+}
+
+public extension TutorialConsumer where Self: UIViewController {
+    public func showTutorial() {
+        if visibleView() {
+            toggleUserInteraction(false)
+            
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.3) {
+                let segueName = self.tutorialNavigationHandler().segue()
+                self.tutorialNavigationHandler().performTipsSegue(named: segueName)
+                
+                self.toggleUserInteraction(true)
+            }
+        }
+    }
+}
+
